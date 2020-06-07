b39f08bb6f18285821e02ab5290c1916f7e655e0
georgekaran <georgekaran12@gmail.com>
2020-06-06 21:44:41 -0300

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -4,4 +4,5 @@ export enum Subjects {
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
   ExpirationComplete = "expiration:complete",
+  PaymentCreated = "payment:created",
 }
