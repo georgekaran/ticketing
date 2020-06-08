5d0262111bd46d542e63bdf365e4eba0d9537806
georgekaran <georgekaran12@gmail.com>
2020-06-08 00:34:58 -0300

    Created new ticket page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,28 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import buildClient from '../api/build-client';
-import Header from '../components/Header'
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/Header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className="container">
+        <Component {...pageProps} currentUser={currentUser} />
+      </div>
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
-  const { data } = await client.get('/api/users/currentuser');
+  const { data } = await client.get("/api/users/currentuser");
 
-  let pageProps = {}
+  let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
     pageProps,
-    ...data
+    ...data,
   };
 };
 
---
+++ client/pages/tickets/new.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NewTicket = () => {
+  return (
+    <div>
+      <h1>Create ticket</h1>
+      <form>
+        <div className="form-group">
+          <label>Title</label>
+          <input className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTicket;
